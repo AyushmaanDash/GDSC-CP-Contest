7ac0a862aaac307c4e9f7b59016e7a5ab1921ae4
Ayushmaan Dash <97537316+AyushmaanDash@users.noreply.github.com>
2022-02-05 18:51:14 +0530

    Update Wednesday.cpp

--- Wednesday.cpp
+++ Wednesday.cpp
@@ -1,3 +1,19 @@
+// Find the probability of a day to be Wednesday in a year taking into consideration that leap year occurs once in 4 years
+
+// Input Format
+
+// There is no input for the question.
+
+// Constraints
+
+// -
+
+// Output Format
+
+// Print an integer - the sum of numerator and then the denominator for the probability i.e. if probability is p / q, where p and q are in their lowest form, then print p + q. example if p and q are 1 and 14 then print 15
+
+
+
 #include <iostream>
 using namespace std;
 
